ff8ddab5e260d2db53db0db6b2300547d3208aed
rekan <rekanagy06@gmail.com>
2017-01-12 11:43:39 +0100

    Implemented basic Card() class

---
+++ src/main/java/org/leanpoker/player/Card.java
@@ -0,0 +1,26 @@
+package org.leanpoker.player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Card {
+    private String rank;
+    private int suit;
+
+
+    private static final Map<String, Integer> alphabeticalSuit = new HashMap<String, Integer>() {{
+        put("J", 11);
+        put("Q", 12);
+        put("K", 13);
+        put("A", 14);
+    }};
+
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+
+        if (alphabeticalSuit.get(suit) != null) {
+            this.suit = alphabeticalSuit.get(suit);
+        } else this.suit = Integer.parseInt(suit);
+    }
+}
