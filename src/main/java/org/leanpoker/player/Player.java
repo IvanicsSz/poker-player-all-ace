7fb57265429f65a7112d328552d743444103752d
Ivanics Szilárd <ivanics.szilard@gmail.com>
2017-01-12 11:41:54 +0100

    Jason to string

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,9 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.Map;
 
@@ -9,7 +12,16 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        //JsonElement jsonElement = new JsonParser().parse(request);
+
+
+        JsonObject gameState = request.getAsJsonObject();
+        JsonArray player = gameState.getAsJsonArray("players");
+        JsonObject ace = (JsonObject) player.get(2);
+        String cards = ace.get("hole_cards").toString();
+        System.err.println(cards);
+
+        return 1000;
     }
 
     public static void showdown(JsonElement game) {
--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -11,9 +11,22 @@ public class PlayerTest {
     @Test
     public void testBetRequest() throws Exception {
 
-        JsonElement jsonElement = new JsonParser().parse("{\"key1\": \"value1\", \"key2\": \"value2\"}");
 
-        assertEquals(0, Player.betRequest(jsonElement));
+        String json = "{\"game_state\":" +
+                "{\"tournament_id\":\"586e79d9037db5000403d046\",\"game_id\":\"58774a42c441a9000400001d\",\"round\":0,\"" +
+                "players\":\"" +
+                "[" +
+                "{\"name\":\"Royal Bluff\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"hole_cards\":[],\"time_used\":0,\"version\":\"Default Python folding player +1\",\"id\":0}," +
+                "{\"name\":\"Back Street Boys and Mark\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"hole_cards\":[],\"time_used\":0,\"version\":\"EXCEPTION\",\"id\":1}," +
+                "{\"name\":\"all ace\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"hole_cards\":[],\"time_used\":0,\"version\":\"Default Java folding player\",\"id\":2}," +
+                "{\"name\":\"Ace Of Spades\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"hole_cards\":[],\"time_used\":0,\"version\":\"Default Java folding player\",\"id\":3}," +
+                "{\"name\":\"ZB ZS\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"hole_cards\":[],\"time_used\":0,\"version\":\"_ZB.ZS\",\"id\":4}," +
+                "{\"name\":\"weneverwonanything\",\"stack\":1000,\"status\":\"active\",\"bet\":0,\"hole_cards\":[],\"time_used\":0,\"version\":\"Default Python folding player\",\"id\":5}" +
+                "]," +
+                "\"small_blind\":2,\"big_blind\":4,\"orbits\":0,\"dealer\":4,\"community_cards\":[],\"current_buy_in\":0,\"pot\":0}}";
+        JsonElement jsonElement = new JsonParser().parse(json);
+        //Player.betRequest(new JsonParser().parse(req.getParameter("game_state")));
+        assertEquals(1000, Player.betRequest(jsonElement));
 
     }
 }
