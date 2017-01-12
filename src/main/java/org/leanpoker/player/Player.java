1b64c6729fc9f5f9d8740ccd0886d9fa5e1646cf
Ivanics Szilárd <ivanics.szilard@gmail.com>
2017-01-12 11:58:20 +0100

    cardsuite and card rank

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -19,7 +19,20 @@ public class Player {
         JsonObject gameState = request.getAsJsonObject();
         JsonArray player = gameState.getAsJsonArray("players");
         JsonObject ace = (JsonObject) player.get(2);
-        String cards = ace.get("hole_cards").toString();
+        System.out.println("NAME:" + ace.get("name"));
+        JsonArray cards = ace.getAsJsonArray("hole_cards");
+        String cardRank = null;
+        String cardSuite = null;
+
+        for (int i = 0; i < cards.size(); i++) {
+            JsonObject card = cards.get(i).getAsJsonObject();
+            cardRank = card.get("rank").toString();
+            cardSuite = card.get("suit").toString();
+        }
+        //cards.get(1).
+        //[{"rank":"7","suit":"spades"},{"rank":"4","suit":"diamonds"}]
+        System.out.println("cardSuite = " + cardSuite);
+        System.out.println("cardRank = " + cardRank);
         System.err.println(cards);
         System.err.println(request);
         return 1000;
