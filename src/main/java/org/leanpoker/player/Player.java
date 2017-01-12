8e2743c7076ca5f14bf9d76f8cf2e7e157340a9a
Ivanics Szilárd <ivanics.szilard@gmail.com>
2017-01-12 14:40:28 +0100

    r

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -37,32 +37,30 @@ public class Player {
 
         JsonObject cardRow1 = cards.get(0).getAsJsonObject();
         JsonObject cardRow2 = cards.get(1).getAsJsonObject();
-
-        Card card1 = new Card(cardRow1.get("suit").toString(),cardRow1.get("rank").toString());
-        Card card2 = new Card(cardRow2.get("suit").toString(),cardRow2.get("rank").toString());
-        //cards.get(1).
-        //[{"rank":"7","suit":"spades"},{"rank":"4","suit":"diamonds"}]
-       // System.out.println("card1 = " + card1);
-       // System.out.println("card2 = " + card2);
-        System.err.println(request);
-        System.out.println("checking community cards");
-        //System.err.println(getCommunityCards(gameState));
-        //System.out.println(currentBuyIn-bet+200);
-        System.out.println("NEW LOGIC" + (currentBuyIn-bet+raise));
-        //if (cardRow1.get("rank").getAsInt() != 0 && cardRow1.get("rank").getAsInt() != 0)
+        System.out.println("CHECKPOINT 2.5");
+        if (cardRow1 != null && cardRow2 != null) {
+            Card card1 = new Card(cardRow1.get("suit").toString(), cardRow1.get("rank").toString());
+            Card card2 = new Card(cardRow2.get("suit").toString(), cardRow2.get("rank").toString());
+            //cards.get(1).
+            System.out.println("card1 = " + card1);
+            System.out.println("card2 = " + card2);
+            System.err.println(request);
+            System.out.println("checking community cards");
+            System.out.println("NEW LOGIC" + (currentBuyIn - bet + raise));
 //        if (currentBuyIn < 800) {
 //            result = currentBuyIn-bet+raise;
 //        }   else
 //            result = 0;
-        result = 0;
+            result = 0;
 
-        if (card1.getRank() > 9 && card2.getRank() > 9 && card1.getRank() == card2.getRank()){
-            result = 1000;
+            if (card1.getRank() > 9 && card2.getRank() > 9 && card1.getRank() == card2.getRank()) {
+                result = 1000;
+            }
+
+            // current_buy_in - players[in_action][bet] + minimum_raise
+            System.out.println("CHECKPOINT 3");
         }
-
-       // current_buy_in - players[in_action][bet] + minimum_raise
-        System.out.println("CHECKPOINT 3");
-        return 1000;
+            return 1000;
     }
 
     private static List<Card> getCommunityCards(JsonObject gameState) {
